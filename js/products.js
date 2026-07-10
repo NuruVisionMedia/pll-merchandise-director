5999520a8dca25009b5cb73c91a1531ddff0618c
NuruVisionMedia <mrlonniesimpson@yahoo.com>
2026-07-09 20:08:56 -0700

    Add persistent product management functionality

--- js/products.js
+++ js/products.js
@@ -7,8 +7,28 @@ Products Module
 
 const Products = {
 
+    storageKey: "pll-products",
+
     init() {
         console.log("Products Module Loaded");
+        this.load();
+    },
+
+    load() {
+        const savedProducts = localStorage.getItem(this.storageKey);
+
+        if (savedProducts) {
+            PLL.state.products = JSON.parse(savedProducts);
+        }
+
+        return PLL.state.products;
+    },
+
+    save() {
+        localStorage.setItem(
+            this.storageKey,
+            JSON.stringify(PLL.state.products)
+        );
     },
 
     list() {
@@ -16,8 +36,19 @@ const Products = {
     },
 
     add(product) {
-        PLL.state.products.push(product);
-        console.log("Product Added", product);
+        const newProduct = {
+            id: Date.now(),
+            title: product.title || "Untitled Product",
+            pillar: product.pillar || "TRAIN",
+            status: product.status || "Draft",
+            pllScore: product.pllScore || 0,
+            seoScore: product.seoScore || 0
+        };
+
+        PLL.state.products.push(newProduct);
+        this.save();
+
+        return newProduct;
     },
 
     remove(id) {
@@ -25,7 +56,12 @@ const Products = {
             product => product.id !== id
         );
 
-        console.log("Product Removed", id);
-    }
+        this.save();
+    },
 
+    findById(id) {
+        return PLL.state.products.find(
+            product => product.id === id
+        );
+    }
 };
