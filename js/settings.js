a9d9f9db1d5bf2c40e9e757bd07f98c0846e3457
NuruVisionMedia <mrlonniesimpson@yahoo.com>
2026-07-09 19:42:46 -0700

    Create initial Settings module

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,35 @@
+/*
+==========================================
+PLL MERCHANDISE DIRECTOR
+Settings Module
+==========================================
+*/
 
+const Settings = {
+
+    init() {
+        console.log("Settings Module Loaded");
+    },
+
+    load() {
+        const saved = localStorage.getItem("pll-settings");
+
+        if (saved) {
+            PLL.state.settings = JSON.parse(saved);
+        }
+
+        return PLL.state.settings;
+    },
+
+    save(settings) {
+        PLL.state.settings = settings;
+
+        localStorage.setItem(
+            "pll-settings",
+            JSON.stringify(settings)
+        );
+
+        console.log("Settings Saved");
+    }
+
+};
