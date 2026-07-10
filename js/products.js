60daa1d2cf0ce765f0aa9d758330644566f9ec92
NuruVisionMedia <mrlonniesimpson@yahoo.com>
2026-07-09 19:30:22 -0700

    Create initial Products module

--- js/products.js
+++ js/products.js
@@ -1,1 +1,31 @@
+/*
+==========================================
+PLL MERCHANDISE DIRECTOR
+Products Module
+==========================================
+*/
 
+const Products = {
+
+    init() {
+        console.log("Products Module Loaded");
+    },
+
+    list() {
+        return PLL.state.products;
+    },
+
+    add(product) {
+        PLL.state.products.push(product);
+        console.log("Product Added", product);
+    },
+
+    remove(id) {
+        PLL.state.products = PLL.state.products.filter(
+            product => product.id !== id
+        );
+
+        console.log("Product Removed", id);
+    }
+
+};
